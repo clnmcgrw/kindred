5103840df45e2d40f9d445db67426eb50b6945fc
dougalcantara <dougalcantara619@gmail.com>
2020-01-27 17:47:41 -0800

    some gallery page

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,26 +1,5 @@
-import Isotope from 'isotope-layout';
 import { $doc } from './ui';
 
-const $galleryParent = $('.ks-gallery > .ks-inner');
-const $galleryItems = $galleryParent.find('.ks-gallerycard');
-
-let iso;
-
-function attachEventListeners() {
-  $galleryItems.each(function() {
-    const $t = $(this);
-    const imageWidth = $t.data('image-width');
-
-    const computedWidth = ((imageWidth / $galleryParent.width()) * 100).toFixed(
-      2
-    );
-
-    $t.css({
-      'flex-basis': `${computedWidth}%`,
-    });
-  });
-}
-
 export default () => {
-  attachEventListeners();
+  console.log(window.remainingGalleryItems);
 };
---
+++ src/js/lazyloading.js
@@ -0,0 +1,53 @@
+const targets = document.querySelectorAll('*[data-src]');
+
+function startWatcher(handler) {
+  const opts = {
+    threshold: 0.125,
+  };
+
+  const observer = new IntersectionObserver(handler, opts);
+
+  targets.forEach(el => observer.observe(el));
+}
+
+function checkInView(entries) {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      lazyload(entry.target);
+    }
+  });
+}
+
+function lazyload(element) {
+  const src = element.getAttribute('data-src');
+  const isAlreadyLoaded =
+    element.classList.contains('lazyload--progress') ||
+    element.classList.contains('lazyload--complete');
+
+  /**
+   * the data-src gets removed once lazyload is complete, so we can check
+   * against this attribute to keep from doubling-up
+   */
+  if (isAlreadyLoaded) return;
+
+  const img = new Image();
+
+  element.classList.add('lazyload--progress');
+
+  img.src = src;
+  img.onload = () => {
+    const isBgImg = element.tagName === 'DIV';
+
+    if (isBgImg) {
+      element.setAttribute('style', `background-image: url(${src})`);
+    } else {
+      element.setAttribute('src', src);
+    }
+
+    element.classList.add('lazyload--complete');
+
+    if (typeof onComplete === 'function') onComplete();
+  };
+}
+
+export default () => startWatcher(checkInView);
