ccb28dab3fc4762c64f04c6c6f7c860641422e38
dougalcantara <dougalcantara619@gmail.com>
2020-03-27 12:13:13 -0700

    new homepage carousel

---
+++ src/js/homepage-slider.js
@@ -0,0 +1,77 @@
+import Flickity from 'flickity';
+import 'flickity-fullscreen';
+import { $doc } from './ui';
+
+const $parent = $('.ks-homepageslider');
+const $mainImg = $parent.find('.ks-homepageslider__mainimg img');
+const $carousel = $parent.find('.ks-homepageslider__carousel');
+const $slides = $parent.find('.ks-homepageslider__slide');
+const $prev = $parent.find('.ks-homepageslider__prevtrigger');
+const $next = $parent.find('.ks-homepageslider__nexttrigger');
+const $fullscreen = $parent.find('.ks-homepageslider__fullscreentrigger');
+const $labels = $parent.find('.ks-homepageslider__product');
+
+function setMainImage(idx) {
+  if (!idx) {
+    $mainImg.attr(
+      'src',
+      $carousel
+        .find('img')
+        .first()
+        .attr('src')
+    );
+  } else {
+    $parent.addClass('changing');
+
+    setTimeout(function() {
+      $mainImg.attr(
+        'src',
+        $($slides[idx])
+          .find('img')
+          .attr('src')
+      );
+    }, 200);
+
+    setTimeout(function() {
+      $parent.removeClass('changing');
+    }, 200);
+  }
+}
+
+export default () => {
+  if (!parent) return;
+
+  const flkty = new Flickity($carousel[0], {
+    cellSelector: '.ks-homepageslider__slide',
+    prevNextButtons: false,
+    cellAlign: 'left',
+    pageDots: false,
+    on: {
+      ready() {
+        setMainImage();
+      },
+      staticClick(_, __, ___, i) {
+        setMainImage(i);
+        this.select(i);
+      },
+      change(i) {
+        setMainImage(i);
+        $labels.each((_, label) => $(label).removeClass('visible'));
+        $($labels[i]).addClass('visible');
+      },
+    },
+  });
+
+  $prev.click(() => flkty.previous());
+  $next.click(() => flkty.next());
+  $fullscreen.click(() => $parent.toggleClass('fullscreen'));
+
+  $labels.first().addClass('visible');
+
+  $doc.keydown(({ keyCode }) => {
+    // escape key
+    if (keyCode == 27 && $parent.hasClass('fullscreen')) {
+      $parent.removeClass('fullscreen');
+    }
+  });
+};
